a644a17c3266bbf632f56d4f14c839f50ad02165
Philippe Charrière <ph.charriere@gmail.com>
2022-06-26 20:00:45 +0000

    📝 save

--- 90-cows/cows/src/main.rs
+++ 90-cows/cows/src/main.rs
@@ -1,4 +1,6 @@
 
+use std::collections::LinkedList;
+use std::{thread, time::Duration};
 
 
 #[derive(Clone, Copy)]
@@ -11,10 +13,11 @@ struct Constraints {
 
 #[derive(Clone, Copy)]
 struct Cow {
-  //nick_name: String,
   id: u64,
   x: f64,
   y: f64,
+  former_x: f64,
+  former_y: f64,
   constraints: Constraints,
   x_velocity: f64,
   y_velocity: f64
@@ -22,24 +25,16 @@ struct Cow {
 
 // associated function
 impl Cow {
-
-  pub fn move_right(&mut self) {
-    self.x = self.x + 1.0;
+  fn distance(&mut self, boid: Cow) -> f64 {
+    let dist_x = self.x - boid.x;
+    let dist_y = self.y - boid.x;
+    (dist_x*dist_x + dist_y*dist_y).sqrt()
   }
-
-  pub fn something_to_do(&mut self, mut boids: Vec<Cow>) {
-    println!("{}", boids.len());
-    self.x = self.x + 1.0;
-  }
-
-
-
-
-
 }
 
 fn main() {
-  let mut cows_list: Vec<Cow> = vec![];
+
+  let mut cows_list: LinkedList<Cow> = LinkedList::new();
 
   let constraints = Constraints {
     border:      5.0,
@@ -48,51 +43,290 @@ fn main() {
     max_velocity: 5.0
   };
 
-  let mut bob = Cow {
-    id: 99,
-    x:10.0,
-    y:10.0,
-    constraints: constraints,
-    x_velocity: 1.0,
-    y_velocity: -1.0
-};
-
 
   let mut i = 0;
 
-  while i < 5 {
+  while i < 55 {
       let cow = Cow {
         id:i,
         x:10.0,
         y:10.0,
+        former_x:10.0,
+        former_y:10.0,
         constraints: constraints,
         x_velocity: 1.0,
         y_velocity: -1.0,
       };
       i = i + 1;
-      cows_list.push(cow)
+      cows_list.push_back(cow)
   }
 
+
   let mut index = 0;
-
-
   while index < 5 {
+    //============================================
 
-    for cow in cows_list.iter_mut() {
+    let cows_list_move_with: LinkedList<Cow> = cows_list.iter().map(|cow| {
 
-      let cows_list_bis: Vec<Cow> = cows_list.clone();
+      let mut current_cow = Cow { // self
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: cow.x_velocity,
+        y_velocity: cow.y_velocity,
+      };
 
-      cow.move_right();
-      //cow.something_to_do(cows_list);
+      let mut avg_x = 0.0;
+      let mut avg_y = 0.0;
 
-      //println!("{}", cow.distance(bob));
+      // calculate the distance
+      cows_list.iter().for_each(|cow| {
+        if cow.x == current_cow.x && cow.y == current_cow.y {
+          //println!("1️⃣");
+          if current_cow.distance(*cow) > 300.0 {
+            //println!("2️⃣");
+            avg_x += cow.x_velocity;
+            avg_y += cow.y_velocity;
+          }
+        }
+      });
 
-      println!("{}: {} {}", cow.id, cow.x, cow.y);
+      avg_x /= cows_list.len() as f64;
+      avg_y /= cows_list.len() as f64;
+
+      //println!("3️⃣ {} {}",avg_x, avg_y);
+
+      let avg_distance = (avg_x*avg_x+avg_y*avg_y).sqrt() * 1.0;
+
+      //println!("🖐️ avg_distance {}", avg_distance);
+
+      if avg_distance == 0.0 {
+
+      } else {
+        current_cow.x_velocity = (current_cow.x_velocity + (avg_x/avg_distance) * 0.05).min(current_cow.constraints.max_velocity);
+        current_cow.y_velocity = (current_cow.y_velocity + (avg_y/avg_distance) * 0.05).min(current_cow.constraints.max_velocity);
+
+        //println!("🖐️ x_velocity {} y_velocity {}",current_cow.y_velocity, current_cow.y_velocity);
+      }
+
+      Cow {
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: current_cow.x_velocity,
+        y_velocity: current_cow.y_velocity,
+      }
+    }).collect();
+
+    /*
+    cows_list_move_with.iter().for_each(|cow| {
+      println!("🤖 {}: {} {}", cow.id, cow.x, cow.y);
+    });
+    */
+
+    let cows_list_move_closer: LinkedList<Cow> = cows_list_move_with.iter().map(|cow| {
+
+      let mut current_cow = Cow { // self
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: cow.x_velocity,
+        y_velocity: cow.y_velocity,
+      };
+
+      let mut avg_x = 0.0;
+      let mut avg_y = 0.0;
+
+      // calculate the distance
+      cows_list.iter().for_each(|cow| {
+        if cow.x == current_cow.x && cow.y == current_cow.y {
+          //println!("1️⃣");
+          if current_cow.distance(*cow) > 300.0 {
+            //println!("2️⃣");
+            //avg_x += cow.x_velocity;
+            //avg_y += cow.y_velocity;
+
+            avg_x += current_cow.x - cow.x;
+            avg_y += current_cow.y - cow.y;
+          }
+        }
+      });
+
+      avg_x /= cows_list.len() as f64;
+      avg_y /= cows_list.len() as f64;
+
+      //println!("3️⃣ {} {}",avg_x, avg_y);
+
+      //let avg_distance = (avg_x*avg_x+avg_y*avg_y).sqrt() * 1.0;
+      let avg_distance = (avg_x*avg_x+avg_y*avg_y).sqrt() * -1.0;
 
 
-    };
+      //println!("🖐️ avg_distance {}", avg_distance);
+
+      if avg_distance == 0.0 {
+
+      } else {
+        //current_cow.x_velocity = (current_cow.x_velocity + (avg_x/avg_distance) * 0.05).min(current_cow.constraints.max_velocity);
+        //current_cow.y_velocity = (current_cow.y_velocity + (avg_y/avg_distance) * 0.05).min(current_cow.constraints.max_velocity);
+        current_cow.x_velocity = (current_cow.x_velocity + (avg_x/avg_distance) * 0.15).min(current_cow.constraints.max_velocity);
+        current_cow.y_velocity = (current_cow.y_velocity + (avg_y/avg_distance) * 0.15).min(current_cow.constraints.max_velocity);
+
+        //println!("🖐️ x_velocity {} y_velocity {}",current_cow.y_velocity, current_cow.y_velocity);
+      }
+
+      Cow {
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: current_cow.x_velocity,
+        y_velocity: current_cow.y_velocity,
+      }
+
+    }).collect();
+
+    /*
+    cows_list_move_closer.iter().for_each(|cow| {
+      println!("🤖 {}: {} {}", cow.id, cow.x, cow.y);
+    });
+    */
 
 
+    let cows_list_move_away: LinkedList<Cow> = cows_list_move_closer.iter().map(|cow| {
+
+      let mut current_cow = Cow { // self
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: cow.x_velocity,
+        y_velocity: cow.y_velocity,
+      };
+
+      let mut distance_x = 0.0;
+      let mut distance_y = 0.0;
+      let mut num_close = 0.0;
+
+      cows_list.iter().for_each(|cow| {
+
+        if cow.x == current_cow.x && cow.y == current_cow.y {
+          let min_distance= 15.0;
+          let distance = current_cow.distance(*cow);
+
+          if distance < min_distance {
+            num_close +=1.0;
+            let mut xdiff = current_cow.x - cow.x;
+            let mut ydiff = current_cow.y - cow.y;
+
+            if xdiff >= 0.0 {
+                xdiff = min_distance.sqrt() - xdiff;
+            } else if xdiff < 0.0 {
+                xdiff = -min_distance.sqrt() - xdiff;
+            }
+
+            if ydiff >= 0.0 {
+                ydiff = min_distance.sqrt() - ydiff;
+            } else if ydiff < 0.0 {
+                ydiff = -min_distance.sqrt() - ydiff;
+            }
+            distance_x += xdiff;
+            distance_y += ydiff;
+          }
+          if num_close == 0.0 {
+
+          } else {
+            current_cow.x_velocity -= distance_x / 5.0;
+            current_cow.y_velocity -= distance_y / 5.0;
+          }
+        }
+
+      });
+
+      Cow {
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: current_cow.x_velocity,
+        y_velocity: current_cow.y_velocity,
+      }
+
+    }).collect();
+
+    /*
+    cows_list_move_away.iter().for_each(|cow| {
+      println!("🤖 {}: {} {}", cow.id, cow.x, cow.y);
+    });
+    */
+
+    let cows_list_moving: LinkedList<Cow> = cows_list_move_away.iter().map(|cow| {
+      let mut current_cow = Cow { // self
+        id:cow.id,
+        x:cow.x,
+        y:cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: cow.constraints,
+        x_velocity: cow.x_velocity,
+        y_velocity: cow.y_velocity,
+      };
+      current_cow.x += current_cow.x_velocity;
+      current_cow.y += current_cow.y_velocity;
+
+      if current_cow.x <= current_cow.constraints.border || current_cow.x >= current_cow.constraints.width - current_cow.constraints.border {
+        current_cow.x -= current_cow.x_velocity;
+        current_cow.x = current_cow.x.max(current_cow.constraints.border);
+        current_cow.x = current_cow.x.min(current_cow.constraints.width - current_cow.constraints.border);
+        current_cow.x_velocity = -current_cow.x_velocity;
+        current_cow.x += current_cow.x_velocity;
+      }
+      if current_cow.y <= current_cow.constraints.border || current_cow.y >= current_cow.constraints.height - current_cow.constraints.border {
+        current_cow.y -= current_cow.y_velocity;
+        current_cow.y = current_cow.y.max(current_cow.constraints.border);
+        current_cow.y = current_cow.y.min(current_cow.constraints.height - current_cow.constraints.border);
+        current_cow.y_velocity = -current_cow.y_velocity;
+        current_cow.y += current_cow.y_velocity
+      }
+
+      Cow {
+        id:current_cow.id,
+        x:current_cow.x,
+        y:current_cow.y,
+        former_x:cow.former_x,
+        former_y:cow.former_y,
+        constraints: current_cow.constraints,
+        x_velocity: current_cow.x_velocity,
+        y_velocity: current_cow.y_velocity,
+      }
+
+    }).collect();
+
+    cows_list_moving.iter().for_each(|cow| {
+
+      println!("🤖 {}: {} {} {} {}", cow.id, cow.x, cow.y, cow.x_velocity, cow.y_velocity);
+
+    });
+
+    cows_list = cows_list_moving;
+
+    thread::sleep(Duration::from_millis(100));
+
+    //============================================
     index+=1;
   }
 
